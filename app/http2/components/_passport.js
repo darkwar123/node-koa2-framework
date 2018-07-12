794131dd584b22b593cbab55fff68009e89e429d
Said Gadzhiev <darkwar123@users.noreply.github.com>
2018-07-13 00:19:46 +0300

    Remove app directory

--- app/http2/components/_passport.js
+++
@@ -1,6 +0,0 @@
-const passport = require('koa-passport');
-
-module.exports = [
-	passport.initialize(),
-	passport.session(),
-];
--- app/http2/interfaces/User.js
+++
@@ -1,9 +0,0 @@
-const redis = require('@redis');
-const mongodb = require('@mongodb');
-const router = require('koa-router')();
-
-router.get('/', async (ctx) => {
-	ctx.body = 'IUser interface';
-});
-
-module.exports = router;
