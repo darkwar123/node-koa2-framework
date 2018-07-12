2db7e3b82111527bf9ee8a3e81f45282f91349d7
Said Gadzhiev <darkwar123@users.noreply.github.com>
2018-07-13 00:29:20 +0300

    Fix

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-require('module-alias/register');
+const moduleAlias = require('module-alias');
+
+moduleAlias.addAliases({
+	'@root'  : __dirname,
+	'@redis': __dirname + '/lib/database/redis',
+	'@mongodb': __dirname + '/lib/database/mongodb',
+	'@config': __dirname + '/lib/components/config',
+	'@log': __dirname + '/lib/components/log',
+});
 
 module.exports = require('./lib/');
