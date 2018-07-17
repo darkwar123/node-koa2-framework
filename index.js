f71ed029201b9c63f8ece78f9833114c040c1add
Said Gadzhiev <darkwar123@users.noreply.github.com>
2018-07-17 17:35:41 +0300

    Some changes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ moduleAlias.addAliases({
 	'@root'  : __dirname,
 	'@redis': __dirname + '/lib/database/redis',
 	'@mongodb': __dirname + '/lib/database/mongodb',
-	'@config': __dirname + '/lib/components/config',
-	'@log': __dirname + '/lib/components/log',
 });
 
 module.exports = require('./lib/');
